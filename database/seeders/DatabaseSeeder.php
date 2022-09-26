906dfaa94e565f9b52ca7ab58b41fef7d66620ed
muhrafiq <muhammadr4fiq@gmail.com>
2022-09-26 21:38:46 +0700

    create database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        \App\Models\Category::factory(9)->create();
+        \App\Models\Post::factory(20)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
