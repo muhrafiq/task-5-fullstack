752eae5d7a0256576a43bbc159b7b356556bec20
muhrafiq <muhammadr4fiq@gmail.com>
2022-09-26 21:38:01 +0700

    add routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\v1\UserAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/register', 'App\Http\Controllers\API\v1\UserAuthController@register');
+Route::post('/login', 'App\Http\Controllers\API\v1\UserAuthController@login');
+
+Route::group(['prefix' => 'v1', 'middleware' => 'auth:api'], function(){
+    Route::apiResource('/post', App\Http\Controllers\API\v1\PostController::class);
 });
